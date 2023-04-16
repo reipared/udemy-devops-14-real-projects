5c9650ddea904339b8303363eac70b3295d24db0
Brian Su <briansu2004@hotmail.com>
2023-04-15 20:49:24 -0400

    $

--- 012-CronjobVaultBackupHelmMinikube/upload.sh
+++ 012-CronjobVaultBackupHelmMinikube/upload.sh
@@ -1,13 +1,3 @@
 #/bin/sh
-ls -trl /backup
-
-VAULT_BACKUP_FILE=/backup/$(ls /backup|grep vault)
-if [ -f "$VAULT_BACKUP_FILE" ] && [ -s "$VAULT_BACKUP_FILE" ]  
-then 
-    echo "$VAULT_BACKUP_FILE exists! Backing up now..." 
-else 
-    echo "Backup fails! Either the file doesn't exist or it is empty. Please check your script!" && exit 2
-fi
-
 #mc alias set local-minio http://$MINIO_ADDR:9000 $MINIO_USERNAME $MINIO_PASSWORD
-mc cp --recursive /backup/* local-minio/$BUCKET_NAME
+mc cp --recursive /tmp/* local-minio/$BUCKET_NAME
