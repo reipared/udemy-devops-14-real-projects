404f6d2fa2b13c68284510dfad6d5000ceddc845
Brian Su <briansu2004@hotmail.com>
2023-04-15 21:07:56 -0400

    $

--- 012-CronjobVaultBackupHelmMinikube/upload.sh
+++ 012-CronjobVaultBackupHelmMinikube/upload.sh
@@ -1,6 +1,8 @@
 #/bin/sh
+ls -trl /vault/data
 #mc alias set local-minio http://$MINIO_ADDR:9000 $MINIO_USERNAME $MINIO_PASSWORD
 #mc cp --recursive /tmp/* local-minio/$BUCKET_NAME
 #mc cp --recursive /* local-minio/$BUCKET_NAME
-mc cp /tmp/policy.hcl local-minio/$BUCKET_NAME
+#mc cp /tmp/policy.hcl local-minio/$BUCKET_NAME
 mc cp --recursive /backup/* local-minio/$BUCKET_NAME
+mc cp --recursive /vault/data/* local-minio/$BUCKET_NAME
